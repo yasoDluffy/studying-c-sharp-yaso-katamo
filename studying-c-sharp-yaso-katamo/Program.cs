4c9dd3d616ab7d1f52dcb79393e8cc19d72f2c7a
yasoDluffy <yaso26849@gmail.com>
2024-05-17 12:46:53 +0300

    task1 – part 1

--- studying-c-sharp-yaso-katamo/Program.cs
+++ studying-c-sharp-yaso-katamo/Program.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using studying_c_sharp_yaso_katamo.task1;
 namespace studying_c_sharp_yaso_katamo
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Level1.RunMe();
+            
         }
     }
 }
