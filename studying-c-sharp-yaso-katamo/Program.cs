e5aca9ccb3080163250c6332a1e072650f7e7834
yasoDluffy <yaso26849@gmail.com>
2024-05-17 11:47:55 +0300

    Add project files.

---
+++ studying-c-sharp-yaso-katamo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_yaso_katamo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
